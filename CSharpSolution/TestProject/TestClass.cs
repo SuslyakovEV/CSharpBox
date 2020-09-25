8318b6de114bcd98a591d65aefd10c91c916d7ec
SyslyakovEV <esuslyakov@gmail.com>
2020-09-25 15:11:13 +0300

    Update

---
+++ CSharpSolution/TestProject/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    internal class Setter
+    {
+        public int Value1 { get; private set; } = 10;
+
+        public void MyDo(int Value1)
+        {
+            this.Value1 = Value1;
+        }
+
+
+    }
+
+}
