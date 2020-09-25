6918e77419e5ca937c50c1acd0fb0bf5dc9f34cb
SyslyakovEV <esuslyakov@gmail.com>
2020-09-25 15:10:39 +0300

    Update

--- CSharpSolution/TestProject/Program.cs
+++ CSharpSolution/TestProject/Program.cs
@@ -7,6 +7,14 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Setter st = new Setter();
+            Console.WriteLine(st.Value1);
+
+            //st.Value1 = 100;
+            st.MyDo(123);
+
+            Console.WriteLine(st.Value1);
+            Console.ReadLine();
         }
     }
 }
