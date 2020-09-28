e49bbf4206631a9dea42027859b52f883fd30783
SyslyakovEV <esuslyakov@gmail.com>
2020-09-28 16:30:40 +0300

    Add Complex Numbers App

---
+++ CSharpSolution/CSHSolution/ComplexNumbers/ComplexN.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexNumbers
+{
+    public class ComplexN
+
+    {
+        public double Imaginary { get; private set; }
+
+        public double Real { get; private set; }
+
+        public ComplexN(double imaginary, double real)
+        {
+            Imaginary = imaginary;
+            Real = real;
+        }
+
+        private ComplexN()
+        {
+
+        }
+
+        public ComplexN Plus(ComplexN other)
+        {
+            var complex = new ComplexN();
+            complex.Imaginary = other.Imaginary + Imaginary;
+            complex.Real = other.Real + Real;
+
+            return complex;
+
+        }
+
+        public ComplexN Minus(ComplexN other)
+        {
+            var complex = new ComplexN();
+            complex.Imaginary = other.Imaginary - Imaginary;
+            complex.Real = other.Real - Real;
+
+            return complex;
+
+        }
+
+    }
+}
---
+++ CSharpSolution/CSHSolution/ComplexNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ComplexNumbers;
+
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexN c1 = new ComplexN(1, 1);
+            ComplexN c2 = new ComplexN(1, 2);
+
+            ComplexN result = c1.Plus(c2);
+            Console.WriteLine($"Result of Plus: Real = {result.Real}; Imaginary={result.Imaginary}");
+
+            result = c1.Minus(c2);
+            Console.WriteLine($"Result of Minus: Real = {result.Real}; Imaginary={result.Imaginary}");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
