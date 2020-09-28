4df4408c155bcd0919827d2db516d79f9172ae54
SyslyakovEV <esuslyakov@gmail.com>
2020-09-28 16:10:18 +0300

    Add FibonacciApp

--- CSharpSolution/CSHSolution/FibonachiApp/Program.cs
+++ CSharpSolution/CSHSolution/FibonachiApp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace FibonachiApp
+namespace Fibon
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the number of Fibonacci numbers you want to generate: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] fibon = new int[n];
+
+            int a0 = 0;
+            int a1 = 1;
+
+            fibon[0] = a0;
+            fibon[1] = a1;
+
+            for (int i=2; i<n; i++)
+            {
+                int a = a0 + a1;
+                fibon[i] = a;
+
+                a0 = a1;
+                a1 = a;
+            }
+
+            Console.WriteLine("Fibonacci numbers: ");
+            foreach (int cur in fibon)
+                Console.Write(cur + " ");
+
+            Console.ReadKey();
         }
+
+
     }
+
+
 }
