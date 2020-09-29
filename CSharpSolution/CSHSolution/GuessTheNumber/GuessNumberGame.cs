931fbd93ac4bce2f4f70e3af753c27537dcb82ce
SyslyakovEV <esuslyakov@gmail.com>
2020-09-29 17:10:47 +0300

    Add game Guess the Number

---
+++ CSharpSolution/CSHSolution/GuessTheNumber/GuessNumberGame.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber
+{
+
+    public enum GuessingPlayer
+    {
+        Human,
+        Machine
+    }
+
+    public class GuessNumberGame
+    {
+        private readonly int max;
+        private readonly int maxTries;
+        private readonly GuessingPlayer guessingPlayer;
+
+        public GuessNumberGame(int max = 100, int maxTries = 5, GuessingPlayer guessingPlayer = GuessingPlayer.Human)
+        {
+            this.max = max;
+            this.maxTries = maxTries;
+            this.guessingPlayer = guessingPlayer;
+        }
+
+        public void Start()
+        {
+            if (guessingPlayer==GuessingPlayer.Human)
+            {
+                HumanGuesses();
+            }
+            else
+            {
+                MachineGuesses();
+            }
+        }
+
+        private void HumanGuesses()
+        {
+            Random rand = new Random();
+            int guessedNumber = rand.Next(0, max);
+
+            int lastGuess = -1;
+            int tries = 0;
+            
+            while(lastGuess!=guessedNumber && tries < maxTries)
+            {
+                Console.WriteLine("Guess the Number: ");
+                lastGuess = int.Parse(Console.ReadLine());
+                if (lastGuess == guessedNumber)
+                {
+                    Console.WriteLine("AWESOME! You Won!");
+                    break;
+                }
+                else if (lastGuess < guessedNumber)
+                {
+                    Console.WriteLine("Almost. But my number is greater!");
+                }
+                else
+                {
+                    Console.WriteLine("Almost. But my number is less!");
+                }
+
+                tries++;
+
+                if (tries == maxTries)
+                {
+                    Console.WriteLine("Sorry. You lost!\nGAME OVER");
+                }
+            }
+        }
+
+        private void MachineGuesses()
+        {
+            Console.WriteLine("Enter a Number that is going to be guessed by a computer");
+
+            int guessedNumber = -1;
+            while (guessedNumber == -1)
+            {
+                int parsedNumber = int.Parse(Console.ReadLine());
+                if (parsedNumber >= 0 && parsedNumber <= this.max)
+                {
+                    guessedNumber = parsedNumber;
+                }
+            }
+
+            int lastGuess = -1;
+            int min = 0;
+            int max = this.max;
+            int tries = 0;
+
+            while (lastGuess != guessedNumber && tries < maxTries)
+            {
+                lastGuess = (min + max) / 2;
+                Console.WriteLine($"DId you guess this number - {lastGuess}?");
+                Console.WriteLine("Y - Yes \nG - Your number is greater \nL - Your number is less");
+
+                string answer = Console.ReadLine();
+
+                if (answer=="Y" || answer=="y")
+                {
+                    Console.WriteLine("Great! I won!");
+                    break;
+                }
+
+                else if (answer=="G" || answer=="g")
+                {
+                    min = lastGuess;
+                }
+                else
+                {
+                    max = lastGuess;
+                }
+
+                if(lastGuess==guessedNumber)
+                {
+                    Console.WriteLine("Do not cheat buddy :)\nI won");
+                }
+
+                tries++;
+                if (tries==maxTries)
+                {
+                    Console.WriteLine("Oops, no tries anymore. I lost. \nThank you for the game");
+                }
+            }
+
+        }
+    }
+}
---
+++ CSharpSolution/CSHSolution/GuessTheNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who is gonna Play? \n1 - Human\n2 - Machine");
+            int player = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many tries?");
+            int tries = int.Parse(Console.ReadLine());
+
+            GuessNumberGame game = new GuessNumberGame();
+
+            switch (player)
+            {
+                case 1: game = new GuessNumberGame(maxTries: tries, guessingPlayer: GuessingPlayer.Human);
+                    break;
+
+                case 2: game = new GuessNumberGame(maxTries: tries, guessingPlayer: GuessingPlayer.Machine);
+                    break;
+
+
+            }
+            
+            Console.Clear();
+            game.Start();
+
+
+            Console.ReadKey();
+
+        }
+    } 
+}
