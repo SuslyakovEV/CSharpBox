eb5d807cf92c7570192922abf79daafc519863e7
SyslyakovEV <esuslyakov@gmail.com>
2020-10-13 13:49:41 +0300

    Add game Виселица

---
+++ CSharpSolution/CSHSolution/HangmanGame/HangmanGame.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HangmanGame
+{
+    public class HangmanGame
+    {
+        private readonly int allowedMisses;
+        private bool[] openIndexes;
+        private int triesCounter = 0;
+        private string triedLetters;
+
+        public GameStatus GameStatus { get; private set; } = GameStatus.NotStarted;
+
+        public string Word { get; private set; }
+
+        public string TriedLetters
+        {
+            get
+            {
+                var chars = triedLetters.ToCharArray();
+                Array.Sort(chars);
+                return new string(chars);
+            }
+        }
+
+        public int RemainingTries
+        { get
+            {
+                return allowedMisses - triesCounter;
+            }
+        }
+
+
+        public HangmanGame(int allowedMisses = 6)
+        {
+            if(allowedMisses<5 || allowedMisses>8)
+            {
+                throw new ArgumentException("Number of allowed misses should be between 5 and 8");
+                
+            }
+
+            this.allowedMisses = allowedMisses;
+        }
+
+        public string GenerateWord()
+        {
+            string[] words = File.ReadAllLines("WordsStockRus.txt");
+            Random r = new Random(DateTime.Now.Millisecond);
+
+            int randomIndex = r.Next(words.Length - 2);
+
+            Word = words[randomIndex];
+
+            openIndexes = new bool[Word.Length];
+
+            GameStatus = GameStatus.InProgress;
+
+            return Word;
+        }
+
+        public string GuessLetter(char letter)
+        {
+            if (triesCounter == allowedMisses)
+            {
+                throw new InvalidOperationException($"Exceeded the MAX misses number: {allowedMisses}");
+            }
+
+            if (GameStatus != GameStatus.InProgress)
+            {
+                throw new InvalidOperationException($"Inaproppriate status of the game: {GameStatus}");
+            }
+
+            bool openAny = false;
+            string result = string.Empty;
+
+            for (int i = 0; i<Word.Length; i++)
+            {
+                if (Word[i] == letter)
+                {
+                    openAny = true;
+                    openIndexes[i] = true;
+                }
+
+                if (openIndexes[i])
+                {
+                    result += Word[i];
+                }
+                else
+                {
+                    result += "-";
+                }
+            }
+
+            if (!openAny)
+            {
+                triesCounter++;
+            }
+
+            triedLetters += letter;
+
+            if (IsWin())
+            {
+                GameStatus = GameStatus.Won;
+            }
+            else if(triesCounter==allowedMisses)
+            {
+                GameStatus = GameStatus.Lost;
+            }
+
+            return result;
+
+        }
+
+        private bool IsWin()
+        {
+            foreach (var cur in openIndexes)
+            {
+                if (cur == false)
+                    return false;
+
+            }
+            return true;
+        }
+    }
+}
---
+++ CSharpSolution/CSHSolution/HangmanGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HangmanGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the HangMan Game!");
+            HangmanGame game = new HangmanGame();
+
+            string word = game.GenerateWord();
+
+            Console.WriteLine($"the word consists of {word.Length} letters");
+            Console.WriteLine("Try to guess the word in Russian Language");
+            Console.WriteLine("Press any Key to Start the Game");
+            Console.ReadLine();
+
+            while (game.GameStatus == GameStatus.InProgress)
+            {
+                //Console.Clear();
+                Console.WriteLine("Pick a Letter");
+                char c = Console.ReadLine().ToCharArray()[0];
+
+                string curState = game.GuessLetter(c);
+                Console.WriteLine("****************\nGuessing word:");
+                Console.WriteLine(curState);
+
+                Console.WriteLine($"Remaining tries = {game.RemainingTries}");
+                Console.WriteLine($"Tried letters: {game.TriedLetters}");
+                //Console.WriteLine("****************\nGuessing word:");
+
+            }
+
+            if (game.GameStatus == GameStatus.Lost)
+            {
+                Console.WriteLine($"You are hanged.\nThe word was: {game.Word}");
+            }
+            else if (game.GameStatus == GameStatus.Won)
+            {
+                Console.WriteLine("\n###################");
+                Console.WriteLine("#You Won! Congrats#");
+                Console.WriteLine("###################");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
